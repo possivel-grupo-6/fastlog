af66f4c8cec41f7db8de1b8dfd2ff0900620e453
Joao-Miziaras <99813188+Joao-Miziaras@users.noreply.github.com>
2024-11-12 09:38:27 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,107 +1,159 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-1"  
 }
 
-
-resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr
-
+resource "aws_vpc" "k8s_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
   tags = {
-    Name = "${var.client}-vpc"
+    Name = "k8s-vpc"
   }
 }
 
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "${var.client}-igw"
-  }
-}
-
-resource "aws_subnet" "public" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = var.public_subnet_cidr
-  availability_zone       = var.availability_zone
+resource "aws_subnet" "k8s_subnet_public_a" {
+  vpc_id = aws_vpc.k8s_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
   map_public_ip_on_launch = true
-
   tags = {
-    Name = "${var.client}-public-subnet"
+    Name = "k8s-subnet-public-a"
   }
 }
 
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
+resource "aws_subnet" "k8s_subnet_public_b" {
+  vpc_id = aws_vpc.k8s_vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "k8s-subnet-public-b"
+  }
+}
+resource "aws_internet_gateway" "k8s_igw" {
+  vpc_id = aws_vpc.k8s_vpc.id
+  tags = {
+    Name = "k8s-igw"
+  }
+}
+
+resource "aws_route_table" "k8s_public_rt" {
+  vpc_id = aws_vpc.k8s_vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
+    gateway_id = aws_internet_gateway.k8s_igw.id
   }
   tags = {
-    Name = "${var.client}-public-rt"
+    Name = "k8s-public-rt"
   }
 }
-resource "aws_route_table_association" "rt_assoc_subnet_1" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public.id
+
+resource "aws_route_table_association" "k8s_public_rt_assoc_a" {
+  subnet_id = aws_subnet.k8s_subnet_public_a.id
+  route_table_id = aws_route_table.k8s_public_rt.id
 }
 
-resource "aws_security_group" "instance_sg" {
-  vpc_id = aws_vpc.main.id
+resource "aws_route_table_association" "k8s_public_rt_assoc_b" {
+  subnet_id = aws_subnet.k8s_subnet_public_b.id
+  route_table_id = aws_route_table.k8s_public_rt.id
+}
 
+
+resource "aws_security_group" "k8s_security_group" {
+  vpc_id = aws_vpc.k8s_vpc.id
+  name        = "k8s-security-group"
+  description = "Allow traffic for Kubernetes cluster"
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 6443
+    to_port     = 6443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+resource "aws_security_group" "application_security_group" {
+  vpc_id = aws_vpc.k8s_vpc.id
+  name        = "application-security-group"
+  description = "Allow traffic for apis"
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "ingress_controller_sg" {
+  vpc_id = aws_vpc.k8s_vpc.id
+  name        = "ingress-controller-sg"
+  description = "Allow traffic for Ingress Controller"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_instance" "k8s_worker_a" {
+  ami                         = "ami-005fc0f236362e99f"  
+  instance_type               = "t2.medium"
+  subnet_id                   = aws_subnet.k8s_subnet_public_a.id
+  key_name                    = "k8-key"  
+  iam_instance_profile        = "LabInstanceProfile"
+  vpc_security_group_ids      = [ aws_security_group.application_security_group.id, aws_security_group.ingress_controller_sg.id, aws_security_group.k8s_security_group.id ]
+
 
   tags = {
-    Name = "${var.client}-sg"
+    Name = "k8s-worker-a"
   }
 }
 
 
-
-resource "aws_instance" "bastion-prod" {
-  ami                    = var.ec2_ami_id
-  instance_type          = "t3.small"
-  subnet_id              = aws_subnet.public.id
-  key_name               = "ssh-key"
-  iam_instance_profile   = var.ec2_iam_instance_profile
-  vpc_security_group_ids = [aws_security_group.instance_sg.id]
-
-  tags = {
-    Name = "Docker-node"
-  }
-
-  user_data = <<-EOF
-    #!/bin/bash
-    sudo apt update -y
-    sudo apt install -y docker.io python3 python3-pip git
-    sudo pip3 install docker-compose
-  EOF
-}
-
-
-resource "aws_s3_bucket" "bucket_logs" {
-  bucket = "bucket-${var.client}-raw"
-  acl    = "private"
+resource "aws_instance" "k8s_worker_b" {
+  ami                         = "ami-005fc0f236362e99f" 
+  instance_type               = "t2.medium"
+  subnet_id                   = aws_subnet.k8s_subnet_public_b.id
+  iam_instance_profile        = "LabInstanceProfile"
+  key_name                    = "k8-key"  
+  vpc_security_group_ids      = [ aws_security_group.application_security_group.id, aws_security_group.ingress_controller_sg.id, aws_security_group.k8s_security_group.id ]
 
   tags = {
-    Name = "bucket-${var.client}-raw"
+    Name = "k8s-worker-b"
   }
 }
