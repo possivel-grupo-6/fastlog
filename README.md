daa9f2dc0d2c7236467c0196413fc449358b4c6a
joao-miziaras <joao.miziara@sptech.school>
2024-11-12 21:58:49 -0300

    update

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # fastlog
 Repo para uso do laboratorio do projeto de arquitetura em nuvem
+
+Como usar para o kubernetes
+
+Rodar estes comandos no master para pegar os valores
+hash:
+openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | awk '{print $2}'
+Token
+kubeadm token create
+
+Rodar este comando no worker
+      kubeadm join <ip-masternode>:6443 --token <token> \
+	    --discovery-token-ca-cert-hash sha256:<tokenhash> 
--- terraform/var.tf
+++ terraform/var.tf
@@ -1,35 +1,20 @@
-variable "client" {
-  type    = string
-  default = "bio-sentinel"
-}
-
-variable "vpc_cidr" {
-  type    = string
-  default = "10.100.0.0/24"
-}
-
-variable "public_subnet_cidr" {
-  type    = string
-  default = "10.100.0.0/24"
-}
-
 variable "availability_zone" {
   type    = string
-  default = "us-east-1a"
+  default = "us-east-1"
 }
 
 
 variable "ec2_ami_id" {
   type    = string
-  default = "ami-0f9fc25dd2506cf6d"
+  default = "ami-063d43db0594b521b"
 }
 
-variable "ec2_iam_instance_profile" {
+variable "iam_profile" {
   type    = string
   default = "LabInstanceProfile"
 }
 
-variable "ec2_jupyter_password" {
-  type    = string
-  default = "urubu100"
+variable "number_ec2" {
+  type    = number
+  default = 2
 }
