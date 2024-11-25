ccdc77f13acc8849b4fff038174800344d49b78a
joao-miziaras <joao.miziara@sptech.school>
2024-11-25 11:11:24 -0300

    update

--- jobs/teste.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-import os
-
-DATABASE_URL = "mysql+pymysql://fastlog-user:fastlog-passwd@23.20.132.252:3306/fastlog"
-
-engine = create_engine(DATABASE_URL)
-connection = engine.connect()
-print("Conexão bem-sucedida!")
-
