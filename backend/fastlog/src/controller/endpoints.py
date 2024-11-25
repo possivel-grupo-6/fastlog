1582f365b8983cf97915bc9530bfa29e617314ea
joao-miziaras <joao.miziara@sptech.school>
2024-11-25 14:27:07 -0300

    teste

--- backend/fastlog/src/controller/endpoints.py
+++ backend/fastlog/src/controller/endpoints.py
@@ -1,25 +1,17 @@
-from fastapi import FastAPI, APIRouter
-from src.service.delivery_service import get_buy_by_code
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-# Criação da instância do FastAPI
 app = FastAPI()
 
-# Configurar CORS para permitir acesso de qualquer origem
+# Configurar o middleware para permitir tudo
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Permite qualquer origem, você pode restringir para um domínio específico
-    allow_credentials=True,
-    allow_methods=["*"],  # Permite qualquer método (GET, POST, etc.)
-    allow_headers=["*"],  # Permite qualquer cabeçalho
+    allow_origins=["*"],  # Permitir qualquer origem
+    allow_credentials=True,  # Permitir envio de cookies e credenciais
+    allow_methods=["*"],  # Permitir todos os métodos (GET, POST, etc.)
+    allow_headers=["*"],  # Permitir todos os cabeçalhos
 )
-
-# Definição do Router
-app_endpoint = APIRouter()
 
 @app_endpoint.get("/buy/{code}")
 async def get_buy(code: str):
     return get_buy_by_code(code)
-
-# Incluindo o Router no FastAPI
-app.include_router(app_endpoint)
